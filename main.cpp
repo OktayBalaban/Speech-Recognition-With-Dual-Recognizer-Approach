c4ed4aa39d9122651d57a789947c284be7bbacf2
Oktay <47466287+OktayBalaban@users.noreply.github.com>
2023-07-07 19:12:24 +0300

    Deleting some comments

--- main.cpp
+++ main.cpp
@@ -7,12 +7,6 @@ int main()
     MainManager mainManager;
     mainManager.Init();
     
-    /*
-    VoskModel* model = vosk_model_new(R"(C:\Users\oktay\source\repos\SpeechRecognition\x64\Debug\model\vosk-model-en-us-0.22)");
-    VoskRecognizer* recognizer = vosk_recognizer_new(model, 16000.0);
-    vosk_recognizer_free(recognizer);
-    vosk_model_free(model);
-    */
     return 0;
 }
 
